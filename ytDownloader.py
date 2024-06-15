4a9284932d409bfe7ca341928eef6d92f639bbed
João <joaopedronoronhadutra@gmail.com>
2024-06-15 12:08:57 -0300

    Playlists and videos

--- ytDownloader.py
+++ ytDownloader.py
@@ -1,18 +1,33 @@
 from tkinter import *
 from tkinter import ttk
 from pytube import YouTube
+from pytube import Playlist
 
 def videoDownload(*args):
 
     SAVE_PATH = "C:/Users/João/OneDrive/João Pedro/Vídeos"
+    SAVE_PATH_PLAYLIST = "C:/Users/João/OneDrive/João Pedro/Vídeos/Workout"
 
     try:
+        if option.get() == "video":
+            url = str(yt.get())
+            video = YouTube(url)
 
-        url = str(yt.get())
-        video = YouTube(url)
+            ttk.Label(mainframe, text=video.title).grid(column=2, row=3, sticky=(W,E))
+            video.streams.first().download(SAVE_PATH)
 
-        ttk.Label(mainframe, text=video.title).grid(column=2, row=3, sticky=(W,E))
-        video.streams.first().download(SAVE_PATH)
+        else:
+
+            url = str(yt.get())
+            playlist = Playlist(url)
+            ttk.Label(mainframe, text=playlist.title).grid(column=2, row=3, sticky=(W,E))
+            
+            for video in playlist.videos:
+                try:
+                    ttk.Label(mainframe, text=playlist.title).grid(column=2, row=3, sticky=(W,E))
+                    video.streams.first().download(SAVE_PATH_PLAYLIST)
+                except:
+                    ttk.Label(mainframe, text=f"*VIDEO* {video.title} *UNAVAILABLE*").grid(column=2, row=3, sticky=(W,E))
 
     except:
 
@@ -23,7 +38,19 @@ root = Tk()
 root.name = "YouTube Dowloader"
 
 mainframe = ttk.Frame(root, padding="3 3 12 12")
-mainframe.grid(column=0, row=0, sticky=(N,W,E,S))
+mainframe.grid(column=1, row=1, sticky=(N,W,E,S))
+
+option = StringVar()
+option.set("video")
+
+frame1 = ttk.Frame(mainframe, padding="3 3 4 4")
+frame1.grid(column=1, row=1, sticky=(N,S,W,E))
+
+videoButton = ttk.Radiobutton(frame1, text = "Video", variable=option, value="video")
+playlistButton = ttk.Radiobutton(frame1, text = "Playlist", variable=option, value="playlist")
+
+videoButton.grid(column=1, row=1, sticky=(W))
+playlistButton.grid(column=1, row=3, sticky=(W))
 
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
@@ -44,5 +71,3 @@ root.bind("<Return>", videoDownload)
 
 
 root.mainloop()
-
-
